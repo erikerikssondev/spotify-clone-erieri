88389b08d72cc8597266c566e8ba6dd38741d0af
00erieri <16erieri@gmail.com>
2022-06-14 16:59:58 +0200

    Commit lektion 1

---
+++ src/components/App/App.js
@@ -0,0 +1,13 @@
+import { Button, Typography } from "@mui/material";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Typography sx={{ color: "primary.main" }}>Spotify Clone</Typography>
+      <Button variant="contained">Click me</Button>
+    </div>
+  );
+}
+
+export default App;
